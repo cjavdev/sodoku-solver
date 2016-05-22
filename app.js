32b10259e1b73889c7a074a339fd34e513898ec7
CJ Avilla <cjavilla@gmail.com>
2016-05-22 13:48:03 -0700

    make test board harder to solve

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
+import _ from 'lodash';
 import {Board} from './js/board.js';
 import {Solve} from './js/solve.js';
 
-var COMPLETE_BOARD = [
-  [4, 8, 3, 9, 2, 1, 6, 5, 7],
-  [9, 6, 7, 3, 4, 5, 8, 2, 1],
-  [2, 5, 1, 8, 7, 6, 4, 9, 3],
-  [5, 4, 8, 1, 3, 2, 9, 7, 6],
-  [7, 2, 9, 5, 6, 4, 1, 3, 8],
-  [1, 3, 6, 7, 9, 8, 2, 4, 5],
-  [3, 7, 2, 6, 8, 9, 5, 1, 4],
-  [8, 1, 4, 2, 5, 3, 7, 6, 9],
-  [6, 9, 5, 4, 1, 7, 3, 8, 2]
-];
+// var COMPLETE_BOARD = [
+//   [4, 8, 3, 9, 2, 1, 6, 5, 7],
+//   [9, 6, 7, 3, 4, 5, 8, 2, 1],
+//   [2, 5, 1, 8, 7, 6, 4, 9, 3],
+//   [5, 4, 8, 1, 3, 2, 9, 7, 6],
+//   [7, 2, 9, 5, 6, 4, 1, 3, 8],
+//   [1, 3, 6, 7, 9, 8, 2, 4, 5],
+//   [3, 7, 2, 6, 8, 9, 5, 1, 4],
+//   [8, 1, 4, 2, 5, 3, 7, 6, 9],
+//   [6, 9, 5, 4, 1, 7, 3, 8, 2]
+// ];
 
 var board = new Board(COMPLETE_BOARD);
 console.log('Starting board:');
@@ -22,15 +23,15 @@ if(board.correct()) {
 }
 
 var COMPLETE_BOARD = [
-  [null, null, null, 9, 2, 1, 6, 5, 7],
-  [9, 6, 7, 3, 4, 5, 8, 2, 1],
-  [2, 5, 1, 8, 7, 6, 4, null, 3],
-  [5, null, 8, null, 3, 2, 9, 7, 6],
-  [7, 2, 9, 5, null, 4, 1, null, 8],
-  [1, 3, null, 7, 9, 8, 2, 4, 5],
-  [3, null, 2, 6, 8, 9, 5, 1, 4],
-  [8, 1, 4, 2, null, null, null, 6, 9],
-  [6, 9, 5, 4, 1, 7, 3, 8, 2]
+  [null, null, null, 9, 2, null, null, null, 7],
+  [9, null, 7, null, null, null, 8, 2, null],
+  [2, 5, null, 8, null, null, null, null, 3],
+  [null, null, 8, null, 3, null, 9, null, 6],
+  [7, null, 9, 5, null, 4, null, null, null],
+  [1, 3, null, 7, null, null, 2, null, 5],
+  [3, null, 2, 6, null, 9, 5, null, 4],
+  [null, 1, null, 2, null, null, null, 6, null],
+  [6, 9, 5, null, 1, 7, null, 8, 2]
 ];
 
 var board = new Board(COMPLETE_BOARD);
